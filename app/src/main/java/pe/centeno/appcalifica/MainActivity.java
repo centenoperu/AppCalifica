72e41752123bce3a542ad578691e787733f6f0c4
Richar <rc@centeno.pe>
2018-07-16 15:32:43 -0500

    Initial commit

---
+++ app/src/main/java/pe/centeno/appcalifica/MainActivity.java
@@ -0,0 +1,27 @@
+package pe.centeno.appcalifica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        WebView myWebView = (WebView) findViewById(R.id.WebView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://172.25.38.150/calificacion/index.html");
+
+
+
+
+    }
+}
